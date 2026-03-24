0f37995592fdd2e73a2f8bd46540db398b2209d6
namkungkang <namkungkang@namkungkangui-MacBookAir.local>
2026-03-24 10:57:04 +0900

    사칙연산

--- .vscode/test.c
+++ .vscode/test.c
@@ -2,6 +2,17 @@
 
 int main(void)
 {
-    printf("hello\n");
+   int x = 10;
+   int y = 20;
 
+  int sum = x + y;
+  int diff = x - y;
+  int mul = x * y;
+  int div = x / y;
+
+   printf("x + y = %d\n" ,sum);
+   printf("x - y = %d\n" , diff);
+   printf("x * y = %d\n" ,mul);
+   printf("x / y = %d" ,div);
+   
 }
